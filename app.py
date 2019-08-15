0eff888d7b427835d2c52bb2ff7fa27c81981d35
vincent9579 <vincent9579@gmail.com>
2019-08-15 22:25:44 +0800

    make it better

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 import os,sys,time,platform
 from flask import Flask, request, abort
 app = Flask(__name__)
-app@@app.route('/')
 @app.route("/")
 def hello():
     return "如果你看到這則訊息\n代表上傳成功\n可以開心使用您的LineBot了"
